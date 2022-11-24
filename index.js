5cf10e99b1b34d95f797e501020f0736dcf45e03
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-11-24 23:10:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// ...middleWare...
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to phone garage!')
+})
+
+// ....mongodb connection..
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xazyemr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
