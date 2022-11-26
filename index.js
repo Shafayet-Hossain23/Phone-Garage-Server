329a216094c25fd906afb93e5f7471720a66e00e
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-11-26 08:33:54 +0600

    bookingCollectionByID

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // ...middleWare...
 app.use(cors());
@@ -126,6 +126,14 @@ async function run() {
             const result = await bookingsCollection.find(query).toArray()
             res.send(result)
 
+        })
+        //bookingCollection by id 
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            // console.log(id)
+            const query = { _id: ObjectId(id) }
+            const result = await bookingsCollection.findOne(query)
+            res.send(result)
         })
 
     }
